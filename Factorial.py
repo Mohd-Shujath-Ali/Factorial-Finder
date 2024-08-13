8f9df62200283086cfea13993c8bb52e10b4f119
Mohd-Shujath-Ali <mohammedshujathali1131@gmail.com>
2024-08-13 18:30:39 +0530

    Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,5 +1,17 @@
+# Method-1
+
 n= int(input("Enter your number: "))
 product=1
 for i in range(1,n+1):
  product= product * i
 print(f"The Factorial of {n} is {product}")
+
+# Method-2
+
+def factorial(n):
+ if(n==1 or n==0):
+  return 1
+ return n * factorial(n-1)
+
+n = int(input("Enter a number: "))
+print(f"The factorial of this number is: {factorial(n)}")
